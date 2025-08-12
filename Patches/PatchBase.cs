dcba4c2891cc61d043f4d16cb0a190bad366e29b
Wryyyong <28753317+Wryyyong@users.noreply.github.com>
2025-08-12 16:07:24 +0800

    Reformat to split each patch into subclasses of a base; clean up access modifiers

---
+++ Patches/PatchBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace UnityExplorer.AITSF_UnityExplorerHelper;
+
+internal class PatchBase {
+	protected virtual void Init() {}
+
+	internal static void InitAll() {
+		Type tPatchBase = typeof(PatchBase);
+
+		tPatchBase.Assembly.GetTypes()
+			.Where(type => type.IsSubclassOf(tPatchBase))
+			.Select(type => Activator.CreateInstance(type) as PatchBase)
+			.ToList().ForEach(patch => patch.Init());
+	}
+}
---
+++ Patches/ShowMenuInputBlocker.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+
+using Il2CppGame;
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+namespace UnityExplorer.AITSF_UnityExplorerHelper;
+
+[HarmonyPatch]
+internal class ShowMenuInputBlocker : PatchBase {
+	private static UIManager uiManager;
+
+	private static void SceneChange(Scene scene,LoadSceneMode mode) {
+		uiManager = GameObject.Find("GameController").GetComponent<UIManager>();
+
+		ToggleInputs(UI.UIManager.ShowMenu);
+
+		Melon.EasyLog($"UIManager GameObject found ({uiManager})");
+	}
+
+	protected override void Init() =>
+		SceneManager.add_sceneLoaded((UnityEngine.Events.UnityAction<Scene,LoadSceneMode>)SceneChange);
+
+	[HarmonyPatch(typeof(UI.UIManager),nameof(UI.UIManager.ShowMenu),MethodType.Setter)]
+	[HarmonyPostfix]
+	private static void ToggleInputs(bool value) {
+		bool valueNOT = !value;
+
+		foreach (InputDevice device in InputSystem.devices) {
+			device.disabledInRuntime = value;
+
+			Melon.EasyLog($"Device {device.name}.disabledInRuntime set to {value}");
+		}
+
+		uiManager.SetInteractable(valueNOT);
+
+		Melon.EasyLog($"UIManager.SetInteractable set to {valueNOT}");
+	}
+}
