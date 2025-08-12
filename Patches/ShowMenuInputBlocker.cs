e5d5c9baa4da10d33f42ce7e0ceb595b954f0153
Wryyyong <28753317+Wryyyong@users.noreply.github.com>
2025-08-13 00:59:45 +0800

    ShowMenuInputBlocker: retain input blocking while Freecam is active

--- Patches/ShowMenuInputBlocker.cs
+++ Patches/ShowMenuInputBlocker.cs
@@ -11,30 +11,59 @@ namespace UnityExplorer.AITSF_UnityExplorerHelper;
 [HarmonyPatch]
 internal class ShowMenuInputBlocker : PatchBase {
 	private static UIManager uiManager;
+	private static bool inputToggle = false;
+	private static bool isFreeCamEnabled = false;
 
 	protected override void Init() =>
 		SceneManager.add_sceneLoaded((UnityEngine.Events.UnityAction<Scene,LoadSceneMode>)((scene,mode) => {
 			uiManager = GameObject.Find("GameController")?.GetComponent<UIManager>();
 
 			if (uiManager == null) return;
-			Melon.EasyLog($"UIManager GameObject found ({uiManager})");
+			Melon.EasyLog($"UIManager GameObject cached successfully");
 
-			ToggleInputs(UI.UIManager.ShowMenu);
+			EvaluateAndToggle();
 		}));
 
 	[HarmonyPatch(typeof(UI.UIManager),nameof(UI.UIManager.ShowMenu),MethodType.Setter)]
 	[HarmonyPostfix]
-	private static void ToggleInputs(bool value) {
-		bool valueNOT = !value;
+	private static void ShowMenuToggle() => EvaluateAndToggle();
 
+	[HarmonyPatch(typeof(UI.Panels.FreeCamPanel),nameof(UI.Panels.FreeCamPanel.BeginFreecam))]
+	[HarmonyPostfix]
+	private static void BeginFreecam() {
+		isFreeCamEnabled = true;
+
+		EvaluateAndToggle();
+	}
+
+	[HarmonyPatch(typeof(UI.Panels.FreeCamPanel),nameof(UI.Panels.FreeCamPanel.EndFreecam))]
+	[HarmonyPostfix]
+	private static void EndFreecam() {
+		isFreeCamEnabled = false;
+
+		EvaluateAndToggle();
+	}
+
+	private static void EvaluateAndToggle() {
+		bool newStatus = UI.UIManager.ShowMenu || isFreeCamEnabled;
+
+		if (newStatus == inputToggle) return;
+		inputToggle = newStatus;
+
+		ToggleInputs(newStatus);
+	}
+
+	private static void ToggleInputs(bool doDisable) {
 		foreach (InputDevice device in InputSystem.devices) {
-			device.disabledInRuntime = value;
-			Melon.EasyLog($"Device {device.name}.disabledInRuntime set to {value}");
+			device.disabledInRuntime = doDisable;
+			Melon.EasyLog($"Device {device.name}.disabledInRuntime set to {doDisable}");
 		}
 
 		if (uiManager == null) return;
 
-		uiManager.SetInteractable(valueNOT);
-		Melon.EasyLog($"UIManager.SetInteractable set to {valueNOT}");
+		bool doDisableInv = !doDisable;
+
+		uiManager.SetInteractable(doDisableInv);
+		Melon.EasyLog($"UIManager.SetInteractable set to {doDisableInv}");
 	}
 }
