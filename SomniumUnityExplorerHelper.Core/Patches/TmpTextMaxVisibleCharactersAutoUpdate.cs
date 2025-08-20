6b8c5f9750bfdc487c1535377c8e60bd01147456
Wryyyong <28753317+Wryyyong@users.noreply.github.com>
2025-08-20 19:46:33 +0800

    Add TmpTextMaxVisibleCharactersAutoUpdate

---
+++ SomniumUnityExplorerHelper.Core/Patches/TmpTextMaxVisibleCharactersAutoUpdate.cs
@@ -0,0 +1,20 @@
+using System.Text.RegularExpressions;
+
+using Il2CppTMPro;
+
+namespace UnityExplorer.SomniumUnityExplorerHelper;
+
+[HarmonyPatch(typeof(TMP_Text),nameof(TMP_Text.text),MethodType.Setter)]
+internal static class TmpTextMaxVisibleCharactersAutoUpdate {
+	private static readonly Regex regex = new(@"<+.*?>+",RegexOptions.Compiled);
+
+	private static void Postfix(TMP_Text __instance,string __0) {
+		string useText = __0;
+
+		if (__instance.richText)
+			useText = regex.Replace(useText,"");
+
+		SomniumMelon.EasyLog($"{__instance.gameObject.name}: maxVisibleCharacters updated from {__instance.maxVisibleCharacters} to {useText.Length}");
+		__instance.maxVisibleCharacters = useText.Length;
+	}
+}
